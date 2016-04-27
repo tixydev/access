2db660ecc3d6da3e5d6d4ec4da57b29e02823bb0
tixydev <dev@tixy.co.nz>
2016-04-27 21:20:35 +1200

    Scanning Test

--- js/main.js
+++ js/main.js
@@ -14,21 +14,15 @@ $scope.PushButton1_click = function() {$scope.GotoPage( "Scanner" );};
 NeoApp.controller("Scanner_Ctrl", function($scope,$rootScope,$route,$timeout,$filter,$window,$animate) {
 $App.NAB.PageNumber = 2;
 $App.NAB.PageID = "Scanner";
-$scope.PushButton2_click = function() {cordova.plugins.barcodeScanner.scan(
-      function (result) {
-          alert("We got a barcode\n" +
-                "Result: " + result.text + "\n" +
-                "Format: " + result.format + "\n" +
-                "Cancelled: " + result.cancelled);
-      },
-      function (error) {
-          alert("Scanning failed: " + error);
-      }
-   );};
+$scope.PushButton2_click = function() {result = scan();
+$App.barcode = result.text;
+$App.format = result.$App.format;
+$App.cancelled = result.$App.cancelled;
+alert( "Barcode: "+$App.barcode+" / Format: "+$App.format+" / Cancelled: "+$App.cancelled );};
 });
 NeoApp.controller("NewDialog_Ctrl", function($scope,$rootScope,$modalInstance,$filter,$window) {
  $scope.CloseDialog = function() {
   $modalInstance.close();
  };
 });
-var exec=require("cordova/exec");function BarcodeScanner(){this.Encode={TEXT_TYPE:"TEXT_TYPE",EMAIL_TYPE:"EMAIL_TYPE",PHONE_TYPE:"PHONE_TYPE",SMS_TYPE:"SMS_TYPE"};this.format={"all_1D":61918,"aztec":1,"codabar":2,"code_128":16,"code_39":4,"code_93":8,"data_MATRIX":32,"ean_13":128,"ean_8":64,"itf":256,"maxicode":512,"msi":131072,"pdf_417":1024,"plessey":262144,"qr_CODE":2048,"rss_14":4096,"rss_EXPANDED":8192,"upc_A":16384,"upc_E":32768,"upc_EAN_EXTENSION":65536};};BarcodeScanner.prototype.scan=function(successCallback,errorCallback,config){if(config instanceof Array){}else{if(typeof(config)==='object'){config=[config];}else{config=[];}}if(errorCallback==null){errorCallback=function(){};}if(typeof errorCallback!="function"){console.log("BarcodeScanner.scan failure: failure parameter not a function");return;}if(typeof successCallback!="function"){console.log("BarcodeScanner.scan failure: success callback parameter must be a function");return;}exec(successCallback,errorCallback,'BarcodeScanner','scan',config);};BarcodeScanner.prototype.encode=function(type,data,successCallback,errorCallback,options){if(errorCallback==null){errorCallback=function(){};}if(typeof errorCallback!="function"){console.log("BarcodeScanner.encode failure: failure parameter not a function");return;}if(typeof successCallback!="function"){console.log("BarcodeScanner.encode failure: success callback parameter must be a function");return;}exec(successCallback,errorCallback,'BarcodeScanner','encode',[{"type":type,"data":data,"options":options}]);};var barcodeScanner=new BarcodeScanner();module.exports=barcodeScanner;
+function scan(){console.log('scanning');var scanner=cordova.require("cordova/plugin/BarcodeScanner");scanner.scan(function(result)return result;},function(error){console.log("Scanning failed: ",error);});},function encode(){var scanner=cordova.require("cordova/plugin/BarcodeScanner");scanner.encode(scanner.Encode.TEXT_TYPE,"http://www.nhl.com",function(success){alert("encode success: "+success);},function(fail){alert("encoding failed: "+fail);});}
